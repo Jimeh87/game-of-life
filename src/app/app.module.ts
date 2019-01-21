99927b4d2ca299ef3231b8f607774f1455a5bf57
Jim <JLRennie@gmail.com>
2019-01-20 21:10:11 -0600

    useHash instead of normal urls to get around github pages url redirect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -119,7 +119,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, {useHash: true}),
     NgbModule.forRoot(),
     NgxPaginationModule,
     ColorPickerModule,
