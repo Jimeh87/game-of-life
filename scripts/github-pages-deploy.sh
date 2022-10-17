a8c6b3780083c1f5e814a1f5760200ef056d7086
Jim Rennie <JLRennie@gmail.com>
2022-10-16 23:12:12 -0600

    make circleci ignore gh-pages commits

--- scripts/github-pages-deploy.sh
+++ scripts/github-pages-deploy.sh
@@ -52,7 +52,7 @@ echo "$fqdn" > CNAME
 # stage any changes and new files
 git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-git commit --allow-empty -m "Deploying build $CIRCLE_BUILD_NUM" -m "$CIRCLE_BUILD_URL"
+git commit --allow-empty -m "Deploying build $CIRCLE_BUILD_NUM [ci skip]" -m "$CIRCLE_BUILD_URL"
 # and push, but send any output to /dev/null to hide anything sensitive
 git push --force --quiet origin gh-pages > /dev/null 2>&1
 
