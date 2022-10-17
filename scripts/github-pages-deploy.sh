255201ffae88ac23a29ecfad18ecb94ee968fab7
Jim Rennie <JLRennie@gmail.com>
2022-10-16 23:31:03 -0600

    try 4

--- scripts/github-pages-deploy.sh
+++ scripts/github-pages-deploy.sh
@@ -38,7 +38,7 @@ then
     git checkout gh-pages
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
-    git rm -rf .
+    git rm -rf . > /dev/null 2>&1
 else
     git checkout --orphan gh-pages
 fi
@@ -52,7 +52,7 @@ echo "$fqdn" > CNAME
 # stage any changes and new files
 git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-git commit --allow-empty -m "[ci skip] Deploying build $CIRCLE_BUILD_NUM" -m "[ci skip] $CIRCLE_BUILD_URL"
+git commit --allow-empty -m "[ci skip] Deploying build $CIRCLE_BUILD_URL"
 # and push, but send any output to /dev/null to hide anything sensitive
 git push --force --quiet origin gh-pages > /dev/null 2>&1
 
